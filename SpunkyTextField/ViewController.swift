4a96ede1425d1bac95cf585c6f78558b0531680a
Janardan Yri <janardan.yri@me.com>
2015-06-13 11:11:48 -0700

    Initial Commit

---
+++ SpunkyTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpunkyTextField
+//
+//  Created by Wren on 6/13/15.
+//  Copyright (c) 2015 Janardan Yri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
