ce9e1dcd51a99ed873aab31ef174281f37e2bbb7
Janardan Yri <janardan.yri@me.com>
2015-06-13 12:06:46 -0700

    Successfully respond to keyboard events gracefully. Hooray~

--- SpunkyTextField/ViewController.swift
+++ SpunkyTextField/ViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  @IBOutlet weak var spunkyTextField: UITextField!
+  @IBOutlet weak var spunkyTextFieldMinimumDistanceAboveBottomConstraint: NSLayoutConstraint!
+
+  let minimumTextFieldKeyboardGap : CGFloat = 10
+
+  // MARK: - View Lifecycle
+
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+
+    NSNotificationCenter.defaultCenter().addObserver(self,
+      selector: "keyboardWillChangeFrame:",
+      name: UIKeyboardWillChangeFrameNotification,
+      object: nil
+    )
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  override func viewDidDisappear(animated: Bool) {
+    super.viewDidDisappear(animated)
+
+    NSNotificationCenter.defaultCenter().removeObserver(self,
+      name: UIKeyboardWillChangeFrameNotification,
+      object: nil
+    )
   }
 
+  // MARK: - Keyboard
 
+  func keyboardWillChangeFrame(notification: NSNotification) {
+    let info = notification.userInfo!
+
+    let duration : NSTimeInterval = (info[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+
+    let curve = UIViewAnimationCurve(rawValue: (info[UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).integerValue)!
+    let options = UIViewAnimationOptions(rawValue:UInt(curve.rawValue << 16))
+
+    let nextKeyboardRectInScreen = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+    let nextKeyboardRect = self.view.convertRect(nextKeyboardRectInScreen, fromView: nil)
+    let nextKeyboardDistanceAboveBottom = CGRectGetMaxY(self.view.bounds) - CGRectGetMinY(nextKeyboardRect)
+    let nextMinimumDistanceAboveBottomConstraintConstant = nextKeyboardDistanceAboveBottom + self.minimumTextFieldKeyboardGap
+
+    UIView.animateWithDuration(duration,
+      delay: 0,
+      options: options,
+      animations: {
+        self.spunkyTextFieldMinimumDistanceAboveBottomConstraint.constant = nextMinimumDistanceAboveBottomConstraintConstant
+        self.view.layoutIfNeeded()
+      },
+      completion: nil)
+
+  }
 }
 
